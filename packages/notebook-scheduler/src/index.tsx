6211070929b5f97c779f599cbbf3e11bf08e401f
Luciano Resende <lresende@us.ibm.com>
2019-10-13 09:46:26 -0700

    Remove obsolete experiment panel

--- packages/notebook-scheduler/src/index.tsx
+++ packages/notebook-scheduler/src/index.tsx
@@ -14,12 +14,10 @@
  * limitations under the License.
  */
 import {ILayoutRestorer, JupyterFrontEnd, JupyterFrontEndPlugin} from '@jupyterlab/application';
-import {ICommandPalette, WidgetTracker} from "@jupyterlab/apputils";
-import {JSONExt} from "@phosphor/coreutils";
-import {Widget} from "@phosphor/widgets";
+import {ICommandPalette} from "@jupyterlab/apputils";
 
 import {SubmitNotebookButtonExtension} from "./SubmitNotebook";
-import {NotebookExperimentWidget} from "./NotebookExperiments";
+
 
 import '../style/index.css';
 /**
@@ -46,58 +44,6 @@ export const ewai_extension: JupyterFrontEndPlugin<void> = {
       command: 'notebook:submit',
       rank: -0.5
     });
-
-    // Declare a widget variable
-    let notebookExperimentWidget: NotebookExperimentWidget
-
-    setInterval(() => {
-      if(notebookExperimentWidget) {
-        console.log('>>> timer expired... will refresh experiment list')
-        notebookExperimentWidget.update()
-      }
-    }, 10 * 1000);
-
-    // Add an application command
-    const command: string = 'ewai:open-experiments';
-    app.commands.addCommand(command, {
-    label: 'Notebook Experiments',
-    execute: () => {
-      if (!notebookExperimentWidget) {
-        // Create a new widget if one does not exist
-        notebookExperimentWidget = new NotebookExperimentWidget();
-        notebookExperimentWidget.update();
-      }
-      if (!tracker.has(notebookExperimentWidget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(notebookExperimentWidget);
-      }
-      if (!notebookExperimentWidget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.add(notebookExperimentWidget,'main');
-      } else {
-        // Refresh the comic in the widget
-        notebookExperimentWidget.update();
-      }
-
-      // Add as right side panel
-      app.shell.add(notebookExperimentWidget,'right')
-
-      // Activate the widget
-      app.shell.activateById(notebookExperimentWidget.id);
-    }
-  });
-
-  // Add the command to the palette.
-  palette.addItem({ command, category: 'Deep Learning Workspace' });
-
-
-  // Track and restore the widget state
-  let tracker = new WidgetTracker<Widget>({ namespace: 'ewai' });
-  restorer.restore(tracker, {
-    command,
-    args: () => JSONExt.emptyObject,
-    name: () => 'ewai'
-  });
   }
 };
 
