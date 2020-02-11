03af00a814c2b1b73895b8d75f2beb212659c9e0
Kevin Bates <kbates4@gmail.com>
2020-02-10 17:27:24 -0800

    Add initial swagger docs
    This change adds swagger docs for yaml and json at endpoints
    'api/elyra.yaml' and 'api/elyra.json', respectively. Not every
    detail has been provided for a Pipeline definition, but I believe
    its sufficient to get started.
    
    Fixes #249

---
+++ elyra/api/handlers.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+from notebook.base.handlers import APIHandler
+from tornado import gen, web
+
+
+class BaseSpecHandler(web.StaticFileHandler, APIHandler):
+
+    @staticmethod
+    def get_resource_metadata():
+        """Returns the (resource, mime-type) for the handlers spec.
+        """
+        pass
+
+    def initialize(self):
+        web.StaticFileHandler.initialize(self, path=os.path.dirname(__file__))
+
+    @web.authenticated
+    def get(self):
+        return web.StaticFileHandler.get(self, self.get_resource_metadata()[0])
+
+    def get_content_type(self):
+        return self.get_resource_metadata()[1]
+
+
+class JsonSpecHandler(BaseSpecHandler):
+    """Exposes the ability to return specifications from static files"""
+
+    @staticmethod
+    def get_resource_metadata():
+        """Returns the (resource, mime-type) for the handlers spec.
+        """
+        return 'elyra.json', 'application/json'
+
+
+class YamlSpecHandler(BaseSpecHandler):
+    """Exposes the ability to return specifications from static files"""
+
+    @staticmethod
+    def get_resource_metadata():
+        """Returns the (resource, mime-type) for the handlers spec.
+        """
+        return 'elyra.yaml', 'text/x-yaml'
