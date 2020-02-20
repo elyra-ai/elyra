d92bf2fb33897bfe2c42a14b9bb4343e2c70a09b
Kevin Bates <kevin.bates@us.ibm.com>
2020-02-19 18:18:42 -0800

    Convert backend unittests to pytests
    Note: we cannot fully convert the handlers test to pytest the project
    transitions to using jupyter_server over notebook. In the meantime, as
    much pytest functionality as possible has been implemented for those
    tests.
    
    Resolves #268

--- elyra/metadata/tests/test_handlers.py
+++ elyra/metadata/tests/test_handlers.py
@@ -17,37 +17,10 @@ import os
 import shutil
 
 from traitlets.config import Config
-from notebook.utils import url_path_join
-from notebook.tests.launchnotebook import NotebookTestBase, assert_http_error
+from notebook.tests.launchnotebook import NotebookTestBase
 
 from .test_utils import valid_metadata_json, invalid_metadata_json, another_metadata_json, create_json_file
-
-
-class MetadataRestAPI(object):
-    """Wrapper for kernel REST API requests"""
-    def __init__(self, request, namespace, base_url, headers):
-        self.request = request
-        self.namespace = namespace
-        self.base_url = base_url
-        self.headers = headers
-
-    def _req(self, verb, path, body=None):
-        response = self.request(verb, url_path_join('api', 'metadata', self.namespace, path), data=body)
-
-        if 400 <= response.status_code < 600:
-            try:
-                response.reason = response.json()['message']
-            except Exception:
-                pass
-        response.raise_for_status()
-
-        return response
-
-    def get_all(self):
-        return self._req('GET', '')
-
-    def get(self, name):
-        return self._req('GET', name)
+from .conftest import fetch  # FIXME - remove once jupyter_server is used
 
 
 class MetadataHandlerTest(NotebookTestBase):
@@ -55,75 +28,77 @@ class MetadataHandlerTest(NotebookTestBase):
     config = Config({'NotebookApp': {"nbserver_extensions": {"elyra": True}}})
 
     def setUp(self):
-        self.runtime_dir = os.path.join(self.data_dir, 'metadata', 'runtimes')
-        self.bogus_dir = os.path.join(self.data_dir, 'metadata', 'bogus')
+        # The _dir names here are fixtures that should be referenced by the appropriate
+        # test methods once transition to jupyter_server occurs.
+        self.metadata_runtimes_dir = os.path.join(self.data_dir, 'metadata', 'runtimes')
+        self.metadata_bogus_dir = os.path.join(self.data_dir, 'metadata', 'bogus')
 
-        create_json_file(self.runtime_dir, 'valid.json', valid_metadata_json)
-        create_json_file(self.runtime_dir, 'another.json', another_metadata_json)
-        create_json_file(self.runtime_dir, 'invalid.json', invalid_metadata_json)
-
-        self.runtime_api = MetadataRestAPI(self.request,
-                                           namespace='runtimes',
-                                           base_url=self.base_url(),
-                                           headers=self.auth_headers(), )
-
-        self.bogus_namespace_api = MetadataRestAPI(self.request,
-                                                   namespace='bogus',
-                                                   base_url=self.base_url(),
-                                                   headers=self.auth_headers(), )
-
-    def tearDown(self):
-        pass
+        create_json_file(self.metadata_runtimes_dir, 'valid.json', valid_metadata_json)
+        create_json_file(self.metadata_runtimes_dir, 'another.json', another_metadata_json)
+        create_json_file(self.metadata_runtimes_dir, 'invalid.json', invalid_metadata_json)
 
     def test_bogus_namespace(self):
         # Validate missing is not found
-        with assert_http_error(404, "Namespace 'bogus' was not found!"):
-            self.bogus_namespace_api.get('missing')
 
-        self.assertFalse(os.path.exists(self.bogus_dir))
+        # Remove self.request (and other 'self.' prefixes) once transition to jupyter_server occurs
+        r = fetch(self.request, 'api', 'metadata', 'bogus', 'missing',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 404
+        assert "Namespace 'bogus' was not found!" in r.text
+        assert not os.path.exists(self.metadata_bogus_dir)
 
     def test_missing_runtime(self):
         # Validate missing is not found
-        with assert_http_error(404, "Metadata 'missing' in namespace 'runtimes' was not found!"):
-            self.runtime_api.get('missing')
+        r = fetch(self.request, 'api', 'metadata', 'runtimes', 'missing',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 404
+        assert "Metadata 'missing' in namespace 'runtimes' was not found!" in r.text
 
     def test_invalid_runtime(self):
         # Validate invalid throws 404 with validation message
-        with assert_http_error(404, "Schema validation failed for metadata 'invalid'"):
-            self.runtime_api.get('invalid')
+        r = fetch(self.request, 'api', 'metadata', 'runtimes', 'invalid',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 404
+        assert "Schema validation failed for metadata 'invalid'" in r.text
 
     def test_valid_runtime(self):
         # Ensure valid metadata can be found
-        r = self.runtime_api.get('valid')
-        self.assertEqual(r.status_code, 200)
+        r = fetch(self.request, 'api', 'metadata', 'runtimes', 'valid',
+                  base_url=self.base_url(), headers=self.auth_headers())
+
+        assert r.status_code == 200
         metadata = r.json()
-        self.assertTrue('schema_name' in metadata)
-        self.assertEqual(metadata['display_name'], 'valid runtime')
+        assert 'schema_name' in metadata
+        assert metadata['display_name'] == 'valid runtime'
 
     def test_get_runtimes(self):
         # Ensure all valid metadata can be found
-        r = self.runtime_api.get_all()
-        self.assertEqual(r.status_code, 200)
+        r = fetch(self.request, 'api', 'metadata', 'runtimes',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 200
         metadata = r.json()
         assert isinstance(metadata, dict)
-        self.assertEqual(len(metadata), 1)
+        assert len(metadata) == 1
         runtimes = metadata['runtimes']
-        self.assertEqual(len(runtimes), 2)
-        self.assertIn('another', runtimes.keys())
-        self.assertIn('valid', runtimes.keys())
+        assert len(runtimes) == 2
+        assert 'another' in runtimes.keys()
+        assert 'valid' in runtimes.keys()
 
     def test_get_runtimes_empty(self):
         # Delete the metadata dir contents and attempt listing metadata
-        shutil.rmtree(self.runtime_dir)
-        with assert_http_error(404, "Namespace 'runtimes' was not found!"):
-            r = self.runtime_api.get_all()
+        shutil.rmtree(self.metadata_runtimes_dir)
+        r = fetch(self.request, 'api', 'metadata', 'runtimes',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 404
+        assert "Namespace 'runtimes' was not found!" in r.text
 
         # Now create empty namespace
-        os.makedirs(self.runtime_dir)
-        r = self.runtime_api.get_all()
-        self.assertEqual(r.status_code, 200)
+        os.makedirs(self.metadata_runtimes_dir)
+        r = fetch(self.request, 'api', 'metadata', 'runtimes',
+                  base_url=self.base_url(), headers=self.auth_headers())
+        assert r.status_code == 200
         metadata = r.json()
         assert isinstance(metadata, dict)
-        self.assertEqual(len(metadata), 1)
+        assert len(metadata) == 1
         runtimes = metadata['runtimes']
-        self.assertEqual(len(runtimes), 0)
+        assert len(runtimes) == 0
