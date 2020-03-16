b8f4d58fa7f6199586c11dc2e29f0dd37f3d5d60
Martha Cryan <martha.cryan@ibm.com>
2020-03-16 14:25:23 -0500

    Merge with master

---
+++ cypress/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: 'development',
+  // make sure the source maps work
+  devtool: 'eval-source-map',
+  // webpack will transpile TS and JS files
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        // every time webpack sees a TS file (except for node_modules)
+        // webpack will use "ts-loader" to transpile it to JavaScript
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              // skip typechecking for speed
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
