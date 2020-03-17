593b6e5658621f7489047a239b8600eb1a7e7ec1
Karla Spuldaro <karla.spuldaro@ibm.com>
2020-03-17 16:51:32 -0400

    Remove duplicate import to css file

--- packages/application/src/submission.tsx
+++ packages/application/src/submission.tsx
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-import '../style/index.css';
-
 import { showDialog, Dialog } from '@jupyterlab/apputils';
 import { URLExt } from '@jupyterlab/coreutils';
 import { ServerConnection } from '@jupyterlab/services';
