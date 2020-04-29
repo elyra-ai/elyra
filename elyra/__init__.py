7a6cf6ed742ecdf8dda0ad50dd24a62691e10166
Kevin Doyle <kjdoyle@ca.ibm.com>
2020-04-29 03:52:24 -0400

    Show version in jupyter serverextension list (#452)
    Add missing version import statement to show the 
    version for elyra in jupyter serverextension list

--- elyra/__init__.py
+++ elyra/__init__.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from ._version import __version__
+
 from notebook.utils import url_path_join
 
 from .api.handlers import JsonSpecHandler, YamlSpecHandler
