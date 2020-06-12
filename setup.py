5622ac5f726e90330cbb58cf74524b06053bd0f3
Luciano Resende <lresende@us.ibm.com>
2020-06-12 09:48:35 -0700

    Update KFP Notebook to 0.9.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup_args = dict(
         "jupyter_core>=4.0,<5.0",
         "kfp==0.5.1",
         "urllib3==1.24.2",
-        "kfp-notebook>=0.8.2",
+        "kfp-notebook>=0.9.0,<0.10.0",
         "minio>=5.0.7",
         'nbdime>=2.0.0',
         'jupyterlab>=2.0.0',
