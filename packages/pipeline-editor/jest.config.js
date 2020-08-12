d7f42ef595eafae15958def27f3b366e49c4ed21
Martha Cryan <martha.cryan@ibm.com>
2020-08-12 09:27:17 -0500

    Fix lint

--- packages/pipeline-editor/jest.config.js
+++ packages/pipeline-editor/jest.config.js
@@ -13,4 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// eslint-disable-next-line no-undef
 module.exports = require('../../testutils/jest.config');
