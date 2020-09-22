e81b71535f0d0b726986a23e7fbc96f45ba0224d
Martha Cryan <martha.cryan@ibm.com>
2020-09-22 12:25:54 -0500

    Add getAllSchema:

--- packages/application/src/test/application.spec.ts
+++ packages/application/src/test/application.spec.ts
@@ -31,8 +31,78 @@ afterAll(async () => {
 describe('@elyra/application', () => {
   describe('FrontendServices', () => {
     describe('#getSchema', () => {
-      it('should make server request', async () => {
-        console.debug(await FrontendServices.getSchema('code-snippets'));
+      it('should get schema', async () => {
+        expect(await FrontendServices.getSchema('code-snippets')).toEqual([
+          {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            title: 'Code Snippet',
+            name: 'code-snippet',
+            display_name: 'Code Snippet',
+            namespace: 'code-snippets',
+            uihints: {
+              title: 'Code Snippets',
+              icon: 'elyra:code-snippet'
+            },
+            properties: {
+              schema_name: {
+                title: 'Schema Name',
+                description: 'The schema associated with this instance',
+                type: 'string',
+                pattern: '^[a-z][a-z0-9-_]*[a-z0-9]$',
+                minLength: 1
+              },
+              display_name: {
+                title: 'Display Name',
+                description: 'The display name of the Code Snippet',
+                type: 'string',
+                minLength: 1
+              },
+              metadata: {
+                description: 'Additional data specific to this Code Snippet',
+                type: 'object',
+                properties: {
+                  description: {
+                    title: 'Description',
+                    description: 'Code snippet description',
+                    type: 'string'
+                  },
+                  language: {
+                    title: 'Language',
+                    description: 'Code snippet implementation language',
+                    type: 'string',
+                    uihints: {
+                      field_type: 'dropdown',
+                      default_choices: [
+                        'Python',
+                        'Java',
+                        'R',
+                        'Scala',
+                        'Markdown'
+                      ]
+                    },
+                    minLength: 1
+                  },
+                  code: {
+                    title: 'Code',
+                    description: 'Code snippet code lines',
+                    type: 'array',
+                    uihints: {
+                      field_type: 'code'
+                    }
+                  }
+                },
+                required: ['language', 'code']
+              }
+            },
+            required: ['schema_name', 'display_name', 'metadata']
+          }
+        ]);
+      });
+    });
+    describe('#getAllSchema', () => {
+      it('should get all schema', async () => {
+        const schemas = await FrontendServices.getAllSchema();
+        expect(schemas).toHaveLength(3);
       });
     });
   });
