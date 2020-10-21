5a4c95fd7db593357baf87a2255fcea2c5a6071b
Kevin Bates <kbates4@gmail.com>
2020-10-21 09:06:10 -0700

    Transition handler tests to jupyter-server pytest framework

--- elyra/metadata/tests/test_utils.py
+++ elyra/metadata/tests/test_utils.py
@@ -17,6 +17,7 @@ import errno
 import io
 import json
 import os
+import tornado
 
 from jsonschema import ValidationError
 from elyra.metadata import METADATA_TEST_NAMESPACE, Metadata, MetadataStore, FileMetadataStore, \
@@ -175,6 +176,28 @@ def get_instance(instances, field, value):
         if inst[field] == value:
             return inst
     assert False, "Value '{}' for field '{}' was not found in instances!".format(value, field)
+
+
+def expected_http_error(error, expected_code, expected_message=None):
+    """Check that the error matches the expected output error."""
+    e = error.value
+    if isinstance(e, tornado.web.HTTPError):
+        if expected_code != e.status_code:
+            return False
+        if expected_message is not None and expected_message != str(e):
+            return False
+        return True
+    elif any([
+        isinstance(e, tornado.httpclient.HTTPClientError),
+        isinstance(e, tornado.httpclient.HTTPError)
+    ]):
+        if expected_code != e.code:
+            return False
+        if expected_message:
+            message = json.loads(e.response.body.decode())['message']
+            if expected_message != message:
+                return False
+        return True
 
 
 class PropertyTester(object):
