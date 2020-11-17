d94fcfce26e842db4b1852d47598eeebe1497301
Karla Spuldaro <karla.spuldaro@ibm.com>
2020-11-17 16:09:45 -0500

    Move function declarations to end of file as in pipeline.ts

--- tests/integration/codesnippet.ts
+++ tests/integration/codesnippet.ts
@@ -14,81 +14,6 @@
  * limitations under the License.
  */
 
-const openJupyterLab = (): void => {
-  cy.visit('?token=test&reset');
-};
-
-const openCodeSnippetExtension = (): void => {
-  cy.get('.jp-SideBar [title="Code Snippets"]').click();
-  cy.get('.jp-SideBar .lm-mod-current[title="Code Snippets"]');
-};
-
-const clickCreateNewSnippetButton = (): void => {
-  cy.get(
-    '.elyra-metadataHeader-button[title="Create new Code Snippet"]'
-  ).click();
-};
-
-const clickSaveAndCloseButton = (): void => {
-  cy.get(
-    '.elyra-metadataEditor-saveButton > .bp3-form-content > button:visible'
-  ).click();
-};
-
-const closeCurrentTab = (): void => {
-  cy.get('.jp-mod-current > .lm-TabBar-tabCloseIcon:visible').click();
-};
-
-const checkSnippetIsDisplayed = (snippetName: string) => {
-  return cy
-    .get('.elyra-metadata-item')
-    .find('.elyra-expandableContainer-name')
-    .contains(`[Python] ${snippetName}`);
-};
-
-const fillMetadaEditorForm = (name: string): void => {
-  // Name code snippet
-  cy.get('.elyra-metadataEditor-form-display_name').type(name);
-
-  // Select python language from dropdown list
-  cy.get('.elyra-metadataEditor')
-    .find('button.bp3-button.jp-Button')
-    .first()
-    .click();
-  cy.get('button.elyra-form-DropDown-item')
-    .contains('Python')
-    .click();
-
-  // Add snippet code
-  cy.get('.elyra-metadataEditor-code > .bp3-form-content').type(
-    'print("Code Snippet Test")'
-  );
-
-  clickSaveAndCloseButton();
-};
-
-const deleteSnippet = (snippetName: string): void => {
-  // Find element by name
-  const item = checkSnippetIsDisplayed(snippetName);
-
-  // Click on delete button
-  item
-    .parentsUntil('.elyra-metadata-item')
-    .first()
-    .find('button[title="Delete"]')
-    .click();
-
-  // Confirm on dialog
-  cy.get('.jp-Dialog-header').contains(`Delete snippet: ${snippetName}?`);
-  cy.get('button.jp-mod-accept').click();
-
-  // Removing the assertion below for now, it passes locally but not on remote runs
-  // Snippet name is no longer on display list
-  // cy.get('.elyra-metadata-item')
-  //   .find('.elyra-expandableContainer-name')
-  //   .should('not.contain', `[Python] ${snippetName}`);
-};
-
 describe('Test for Code Snippet extension load and render', () => {
   before(() => {
     openJupyterLab();
@@ -227,3 +152,84 @@ describe('Test for Code Snippet display', () => {
     deleteSnippet('test-code-snippet');
   });
 });
+
+// ------------------------------
+// ----- Utility Functions ------
+// ------------------------------
+
+const openJupyterLab = (): void => {
+  // open jupyterlab with a clean workspace
+  cy.visit('?token=test&reset').wait(1000);
+};
+
+const openCodeSnippetExtension = (): void => {
+  cy.get('.jp-SideBar [title="Code Snippets"]').click();
+  cy.get('.jp-SideBar .lm-mod-current[title="Code Snippets"]');
+};
+
+const clickCreateNewSnippetButton = (): void => {
+  cy.get(
+    '.elyra-metadataHeader-button[title="Create new Code Snippet"]'
+  ).click();
+};
+
+const clickSaveAndCloseButton = (): void => {
+  cy.get(
+    '.elyra-metadataEditor-saveButton > .bp3-form-content > button:visible'
+  ).click();
+};
+
+const closeCurrentTab = (): void => {
+  cy.get('.jp-mod-current > .lm-TabBar-tabCloseIcon:visible').click();
+};
+
+const checkSnippetIsDisplayed = (snippetName: string): void => {
+  return cy
+    .get('.elyra-metadata-item')
+    .find('.elyra-expandableContainer-name')
+    .contains(`[Python] ${snippetName}`);
+};
+
+const fillMetadaEditorForm = (name: string): void => {
+  // Name code snippet
+  cy.get('.elyra-metadataEditor-form-display_name').type(name);
+
+  // Select python language from dropdown list
+  cy.get('.elyra-metadataEditor')
+    .find('button.bp3-button.jp-Button')
+    .first()
+    .click();
+  cy.get('button.elyra-form-DropDown-item')
+    .contains('Python')
+    .click();
+
+  // Add snippet code
+  cy.get('.elyra-metadataEditor-code > .bp3-form-content').type(
+    'print("Code Snippet Test")'
+  );
+
+  clickSaveAndCloseButton();
+};
+
+const deleteSnippet = (snippetName: string): void => {
+  // Find element by name
+  const item = checkSnippetIsDisplayed(snippetName);
+
+  // Click on delete button
+  item
+    .parentsUntil('.elyra-metadata-item')
+    .first()
+    .find('button[title="Delete"]')
+    .click();
+
+  // Confirm on dialog
+  cy.get('.jp-Dialog-header').contains(`Delete snippet: ${snippetName}?`);
+  cy.get('button.jp-mod-accept').click();
+
+  // Removing the assertion below for now, it passes locally but not on remote runs
+  cy.wait(500);
+  // Snippet name is no longer on display list
+  cy.get('.elyra-metadata-item')
+    .find('.elyra-expandableContainer-name')
+    .should('not.contain', `[Python] ${snippetName}`);
+};
