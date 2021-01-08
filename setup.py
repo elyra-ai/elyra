8ed0f47f0a52f481b8c7c82785506fae5adf8b9e
Luciano Resende <lresende@us.ibm.com>
2021-01-07 19:29:30 -0800

    Update to Jupyter Server 1.2.0 (#1178)
    After the removal of pytest-jupter from PyPi we need to
    update to latest Jupyter Server release to re-enable our
    CI

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup_args = dict(
         'jsonschema>=3.2.0',
         'jupyter_core>=4.0,<5.0',
         'jupyter_client>=6.1.7',
-        'jupyter_server>=1.0.5',
+        'jupyter_server>=1.2.0',
         'jupyterlab>==3.0.0rc10',
         # 'jupyterlab-git==0.21.1',
         'kfp-notebook>=0.17.0,<0.18.0',
@@ -72,7 +72,7 @@ setup_args = dict(
         'websocket-client',
     ],
     extras_require={
-        'test': ['pytest', 'pytest-tornasync', 'pytest-jupyter'],
+        'test': ['pytest', 'pytest-tornasync'],
     },
     include_package_data=True,
     classifiers=(
