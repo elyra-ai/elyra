2befd98cfee2d25e2a50dcbf4be23fab2e15b3b0
Patrick Titzler <ptitzler@us.ibm.com>
2021-01-20 09:25:32 -0800

    Update python language server dpeendencies (#1211)

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup_args = dict(
         'nbconvert>=5.6.1,<6.0',
         # 'nbdime>=2.0.0',
         'papermill>=2.1.3',
-        'python-language-server[all]',
+        'python-language-server[all]>=0.36.2',
         'requests>=2.9.1,<3.0',
         'rfc3986-validator>=0.1.1',
         'traitlets>=4.3.2',
