01d183cd83adcc658ff79636b44a4b08e215a4ef
Nick Bourdakos <bourdakos1@gmail.com>
2021-01-28 10:54:06 -0500

    Remove uneeded pipeline parse function
    Instead of duplicating the work of the parser, just update the pipeline
    definition with the needed values

--- elyra/cli.py
+++ elyra/cli.py
@@ -36,21 +36,6 @@ async def submit_pipeline(pipeline):
     return response.to_json()
 
 
-def parse(pipeline_definitions, pipeline_object):
-    if 'primary_pipeline' not in pipeline_definitions:
-        raise ValueError("Invalid pipeline: Could not determine the primary pipeline.")
-    if 'pipelines' not in pipeline_definitions:
-        raise ValueError("Invalid pipeline: Pipeline definition not found.")
-
-    primary_pipeline_id = pipeline_definitions['primary_pipeline']
-    primary_pipeline = PipelineParser._get_pipeline_definition(pipeline_definitions, primary_pipeline_id)
-    if not primary_pipeline:
-        raise ValueError("Invalid pipeline: Primary pipeline '{}' not found.".format(primary_pipeline_id))
-
-    PipelineParser()._nodes_to_operations(pipeline_definitions, pipeline_object, primary_pipeline['nodes'])
-    return pipeline_object
-
-
 def prepare_pipeline(pipeline_path, name=None, runtime='local', runtime_config='local'):
     pipeline_abs_path = os.path.join(os.getcwd(), pipeline_path)
     pipeline_dir = os.path.dirname(pipeline_abs_path)
@@ -67,84 +52,40 @@ def prepare_pipeline(pipeline_path, name=None, runtime='local', runtime_config='
                 abs_path = os.path.join(os.getcwd(), pipeline_dir, node["app_data"]["filename"])
                 node["app_data"]["filename"] = abs_path
     
-    pipeline_object = Pipeline(id=id, name=name, runtime=runtime, runtime_config=runtime_config)
+    # NOTE: The frontend just set the info for first pipeline, but shouldn't it
+    # search for the primary pipeline and set that?
+    # Setting `pipeline_definition["pipelines"][0]` for consistency.
+    pipeline_definition["pipelines"][0]["app_data"]["name"] = name
+    pipeline_definition["pipelines"][0]["app_data"]["runtime"] = runtime
+    pipeline_definition["pipelines"][0]["app_data"]["runtime-config"] = runtime_config
 
-    pipeline = parse(pipeline_definition, pipeline_object)
-
-    return pipeline
+    return pipeline_definition
 
 
-# # elyra add runtime
-# # elyra get/list runtimes
-# # elyra remove/delete runtime <runtime>
-# # elyra replace runtime <runtime>
-# # elyra set/use runtime <runtime>
-# # elyra run <path-to.pipeline>
-# # elyra submit <path-to.pipeline> [--name]
+def print_banner(title):
+    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
+    click.echo(Fore.CYAN + "  {}".format(title) + Style.RESET_ALL)
+    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
+    click.echo()
+
+
+def print_info(title, info_list):
+    click.echo(Fore.CYAN + "❯ {}".format(title) + Style.RESET_ALL)
+    for info_item in info_list:
+        if isinstance(info_item, str):
+            click.echo("  {}".format(info_item))
+        else:
+            click.echo("  {}: {}".format(info_item[0], info_item[1]))
+    click.echo()
+
+
+def print_version():
+    print_info("Version", ["elyra {}".format(__version__)])
 
 
 @click.group()
 def cli():
     pass
-
-
-@click.command()
-@click.argument('resource')
-# Runtime configuration settings
-@click.option('--display_name', prompt=True)
-@click.option('--name', prompt=True)
-@click.option('--description', prompt=True)
-@click.option('--tags', prompt=True)
-# Kubeflow Pipelines settings
-@click.option('--api_endpoint', prompt=True)
-@click.option('--user_namespace', prompt=True)
-@click.option('--api_username', prompt=True)
-@click.option('--api_password', prompt=True)
-@click.option('--engine', prompt=True)
-# Cloud Storage settings
-@click.option('--cos_endpoint', prompt=True)
-@click.option('--cos_username', prompt=True)
-@click.option('--cos_password', prompt=True)
-@click.option('--cos_bucket', prompt=True)
-def add(resource):
-    """TODO: Description"""
-    print("add")
-    return
-
-
-@click.command()
-@click.argument('resource')
-def get(resource):
-    """TODO: Description"""
-    print("get")
-    return
-
-
-@click.command()
-@click.argument('resource')
-@click.argument('name')
-def remove(resource, name):
-    """TODO: Description"""
-    print("remove")
-    return
-
-
-@click.command()
-@click.argument('resource')
-@click.argument('name')
-def replace(resource, name):
-    """TODO: Description"""
-    print("replace")
-    return
-
-
-@click.command()
-@click.argument('resource')
-@click.argument('name')
-def use(resource, name):
-    """TODO: Description"""
-    print("use")
-    return
 
 
 @click.command()
@@ -154,42 +95,23 @@ def submit(pipeline_path, name):
     """TODO: Description"""
     click.echo()
 
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "  Elyra Pipeline Submission" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo()
-
-    click.echo(Fore.CYAN + "❯ Version" + Style.RESET_ALL)
-    click.echo("  elyra " + __version__)
-    click.echo()
+    print_banner("Elyra Pipeline Submission")
+    print_version()
     
-    pipeline = prepare_pipeline(pipeline_path, name=name, runtime='kfp', runtime_config='fun')
+    pipeline_definition = prepare_pipeline(pipeline_path, name=name, runtime='kfp', runtime_config='fun')
+    pipeline = PipelineParser().parse(pipeline_definition)
 
-    click.echo(Fore.CYAN + "❯ Info" + Style.RESET_ALL)
-    click.echo("  name: " + pipeline.name)
-    click.echo()
-
-    click.echo(Fore.CYAN + "❯ Runtime" + Style.RESET_ALL)
-    click.echo("  type: " + pipeline.runtime)
-    click.echo("  config: " + pipeline.runtime_config)
-    click.echo()
+    print_info("Info", [("name", pipeline.name)])
+    print_info("Runtime", [("type", pipeline.runtime), ("config", pipeline.runtime_config)])
     
     with yaspin(text="Submitting Pipeline..."):
         os.environ["ELYRA_METADATA_PATH"] = os.path.join(os.path.expanduser("~"), ".elyra")
         msg = asyncio.get_event_loop().run_until_complete(submit_pipeline(pipeline))
 
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "  Elyra Pipeline Submission Complete" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo()
+    print_banner("Elyra Pipeline Submission Complete")
 
-    click.echo(Fore.CYAN + "❯ Run Url" + Style.RESET_ALL)
-    click.echo("  " + msg["run_url"])
-    click.echo()
-
-    click.echo(Fore.CYAN + "❯ Object Storage Output" + Style.RESET_ALL)
-    click.echo("  " + msg["object_storage_path"])
-    click.echo()
+    print_info("Run Url", [msg["run_url"]])
+    print_info("Object Storage Output", [msg["object_storage_path"]])
 
 
 @click.command()
@@ -198,30 +120,17 @@ def run(pipeline_path):
     """TODO: Description"""
     click.echo()
 
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "  Elyra Pipeline Local Run" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo()
+    print_banner("Elyra Pipeline Local Run")
+    print_version()
 
-    click.echo(Fore.CYAN + "❯ Version" + Style.RESET_ALL)
-    click.echo("  elyra " + __version__)
-    click.echo()
-
-    pipeline = prepare_pipeline(pipeline_path)
+    pipeline_definition = prepare_pipeline(pipeline_path)
+    pipeline = PipelineParser().parse(pipeline_definition)
 
     asyncio.get_event_loop().run_until_complete(submit_pipeline(pipeline))
-
-    click.echo()
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "  Elyra Pipeline Local Run Complete" + Style.RESET_ALL)
-    click.echo(Fore.CYAN + "────────────────────────────────────────────────────────────────" + Style.RESET_ALL)
     click.echo()
 
+    print_banner("Elyra Pipeline Local Run Complete")
 
-cli.add_command(add)
-cli.add_command(get)
-cli.add_command(remove)
-cli.add_command(replace)
-cli.add_command(use)
+
 cli.add_command(submit)
 cli.add_command(run)
