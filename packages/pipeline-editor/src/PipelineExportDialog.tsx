5fe7334d00ee536e841a2cf35e59d7497e8e17c4
Alan Chin <akchin@us.ibm.com>
2021-02-12 21:34:03 -0800

    Experimental support of Airflow as a pipeline runtime (#490)
    Adds an additional pipeline processor to support submitting
    pipelines to an Apache Airflow runtime.  This also includes
    a new runtime configuration schema to accommodate
    the specific information required to submit to Airflow as well as
    documentation updates based on the runtime differences. 
    
    Co-authored-by: Luciano Resende <lresende@us.ibm.com>
    Co-authored-by: Karla Spuldaro <karla.spuldaro@ibm.com>
    Co-authored-by: Alex Bozarth <ajbozart@us.ibm.com>

--- packages/pipeline-editor/src/PipelineExportDialog.tsx
+++ packages/pipeline-editor/src/PipelineExportDialog.tsx
@@ -16,23 +16,85 @@
 
 import * as React from 'react';
 
-import { IRuntime } from './PipelineService';
+import {
+  KFP_SCHEMA,
+  IRuntime,
+  ISchema,
+  PipelineService
+} from './PipelineService';
 
-const FILE_TYPES = [
-  { label: 'KFP static configuration file (YAML formatted)', key: 'yaml' },
-  { label: 'KFP domain-specific language Python code', key: 'py' }
+const KFP_FILE_TYPES = [
+  { label: 'KFP domain-specific language Python code', key: 'py' },
+  { label: 'KFP static configuration file (YAML formatted)', key: 'yaml' }
+];
+
+const AIRFLOW_FILE_TYPES = [
+  { label: 'Airflow domain-specific language Python code', key: 'py' }
 ];
 
 interface IProps {
   runtimes: IRuntime[];
+  schema: ISchema[];
 }
 
-export class PipelineExportDialog extends React.Component<IProps> {
+interface IState {
+  runtimePlatform: string;
+  runtimes: IRuntime[];
+  fileTypes: Record<string, string>[];
+}
+
+export class PipelineExportDialog extends React.Component<IProps, IState> {
+  state = {
+    runtimePlatform: KFP_SCHEMA,
+    runtimes: this.props.runtimes,
+    fileTypes: KFP_FILE_TYPES
+  };
+
+  handleUpdate = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    const selectedPlatform = event.target.value;
+    this.updateDisplayOptions(selectedPlatform);
+  };
+
+  updateDisplayOptions = (platformSelection: string): void => {
+    const runtimes = PipelineService.filterRuntimes(
+      this.props.runtimes,
+      platformSelection
+    );
+    this.setState({
+      runtimes: runtimes,
+      fileTypes:
+        platformSelection === KFP_SCHEMA ? KFP_FILE_TYPES : AIRFLOW_FILE_TYPES
+    });
+  };
+
+  componentDidMount(): void {
+    this.updateDisplayOptions(this.state.runtimePlatform);
+  }
+
   render(): React.ReactNode {
-    const { runtimes } = this.props;
+    const { fileTypes, runtimes } = this.state;
+    const { schema } = this.props;
+    PipelineService.sortRuntimesByDisplayName(runtimes);
+
     return (
       <form>
-        <label htmlFor="runtime_config">Runtime Config:</label>
+        <label htmlFor="runtime_platform">Runtime Platform:</label>
+        <br />
+        <select
+          id="runtime_platform"
+          name="runtime_platform"
+          className="elyra-form-runtime-platform"
+          data-form-required
+          defaultValue={this.state.runtimePlatform}
+          onChange={this.handleUpdate}
+        >
+          {schema.map(schema => (
+            <option key={schema.name} value={schema.name}>
+              {schema.display_name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="runtime_config">Runtime Configuration:</label>
         <br />
         <select
           id="runtime_config"
@@ -40,7 +102,7 @@ export class PipelineExportDialog extends React.Component<IProps> {
           className="elyra-form-runtime-config"
           data-form-required
         >
-          {runtimes.map((runtime: any) => (
+          {runtimes.map(runtime => (
             <option key={runtime.name} value={runtime.name}>
               {runtime.display_name}
             </option>
@@ -54,7 +116,7 @@ export class PipelineExportDialog extends React.Component<IProps> {
           className="elyra-form-export-filetype"
           data-form-required
         >
-          {FILE_TYPES.map(filetype => (
+          {fileTypes.map(filetype => (
             <option key={filetype['key']} value={filetype['key']}>
               {filetype['label']}
             </option>
