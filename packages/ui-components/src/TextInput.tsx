872174c8e7e429b550ea7eb3fb0acd7117287fdc
Martha Cryan <martha.cryan@ibm.com>
2021-03-02 15:07:52 -0600

    Switch to using Material UI in metadata editor (#1293)
    This rewrites the metadata editor using 'Material UI' components
    to be consistent with the JupyterLab UI components, which is in the
    process of moving away from blueprint.js toward material UI components.

---
+++ packages/ui-components/src/TextInput.tsx
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2018-2021 Elyra Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  TextField,
+  InputAdornment,
+  IconButton,
+  FormHelperText,
+  Tooltip,
+  withStyles
+} from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
+import * as React from 'react';
+
+export interface ITextFieldProps {
+  label: string;
+  description: string;
+  fieldName: string;
+  defaultValue: string;
+  required: boolean;
+  secure: boolean;
+  error?: boolean;
+  placeholder?: string;
+  handleTextInputChange: (event: any, fieldName: string) => void;
+}
+
+const CustomTooltip = withStyles((theme: any): any => ({
+  tooltip: {
+    fontSize: 13
+  }
+}))(Tooltip);
+
+export const TextInput = (props: ITextFieldProps): any => {
+  const [errorText, setErrorText] = React.useState(null);
+  React.useEffect(() => {
+    if (props.error) {
+      setErrorText(
+        <FormHelperText error> This field is required. </FormHelperText>
+      );
+    }
+  }, [props.error]);
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const toggleShowPassword = (): void => {
+    setShowPassword(!showPassword);
+  };
+
+  const [value, setValue] = React.useState(props.defaultValue);
+
+  React.useEffect(() => {
+    setValue(props.defaultValue);
+  }, [props.defaultValue]);
+
+  return (
+    <div
+      className={`elyra-metadataEditor-formInput ${
+        props.secure ? 'elyra-metadataEditor-secure' : ''
+      }`}
+    >
+      <CustomTooltip title={props.description || ''}>
+        <TextField
+          key={props.fieldName}
+          label={props.label}
+          required={props.required}
+          variant="outlined"
+          error={props.error}
+          onChange={(event: any): void => {
+            props.handleTextInputChange(event, props.fieldName);
+            setValue(event.nativeEvent.target.value);
+          }}
+          placeholder={props.placeholder}
+          value={value ?? ''}
+          type={showPassword || !props.secure ? 'text' : 'password'}
+          InputProps={
+            props.secure
+              ? {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={toggleShowPassword}
+                        onMouseDown={(event: any): void => {
+                          event.preventDefault();
+                        }}
+                        edge="end"
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }
+              : {}
+          }
+          className={`elyra-metadataEditor-form-${props.fieldName}`}
+        />
+      </CustomTooltip>
+      {errorText}
+    </div>
+  );
+};
