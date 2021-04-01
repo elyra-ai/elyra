d8858f1556bfb92ae65b9487a7e068b6c183c5ea
Luciano Resende <lresende@us.ibm.com>
2021-03-31 22:48:26 -0700

    Update KFP Notebook to 0.22.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ settings_path = './etc/config/settings/*.json'
 metadata_path = './etc/config/metadata/runtime-images/*.json'
 
 # kfp_packages = [
-#     'kfp-notebook~=0.21.0',
+#     'kfp-notebook~=0.22.0',
 #     'kfp==1.3.0',
 #     'kfp-tekton==0.6.0',
 #     ]
@@ -90,7 +90,7 @@ setup_args = dict(
         'websocket-client',
         'yaspin',
         # KFP runtime dependencies
-        'kfp-notebook>=0.21.0',
+        'kfp-notebook>=0.22.0',
         'kfp==1.3.0',
         'kfp-tekton==0.6.0',
         # Airflow runtime dependencies
