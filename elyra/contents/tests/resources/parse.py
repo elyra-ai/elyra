354faa65bfd3f4d2df29b2a8e19aaa760bc0bc0e
Kiersten Stokes <kierstenstokes@Kierstens-MBP.attlocal.net>
2021-04-07 14:12:29 -0500

    Add new backend test scripts and resource files

---
+++ elyra/contents/tests/resources/parse.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2018-2021 Elyra Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+Python Script with Environment Variables
+
+This python script contains various environment variables to test the parser
+functionality.
+"""
+
+import os
+
+os.getenv("VAR1")
+os.environ["VAR2"]
+os.environ.get("VAR3")
+
+print(os.environ["VAR4"])
+print(os.getenv("VAR5", 'localhost'))
+
+os.environ["VAR6"] = "value6"
+print(os.environ.get("VAR7", 'value7'))
+
+os.environ["VAR1"] = "newvalue"
---
+++ elyra/contents/tests/test_content_parser.py
@@ -0,0 +1,130 @@
+#
+# Copyright 2018-2021 Elyra Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import pytest
+
+from ..file_parser import FileParser
+
+
+def parse(filepath):
+    operation_filepath = os.path.join(os.path.dirname(__file__), filepath)
+
+    operation = FileParser.get_instance(filepath=operation_filepath)
+    return operation.get_resources()
+
+
+def _get_variable_names(model):
+    return list(model['env_list'].keys())
+
+
+def test_python_notebook():
+    expected_variable_names = ['VAR1', 'VAR2', 'VAR3', 'VAR4', 'VAR5', 'VAR6', 'VAR7']
+    model = parse("resources/parse_python.ipynb")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_r_notebook():
+    expected_variable_names = ['VAR1', 'VAR2', 'VAR3', 'VAR4']
+    model = parse("resources/parse_r.ipynb")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_python_script():
+    expected_variable_names = ['VAR1', 'VAR2', 'VAR3', 'VAR4', 'VAR5', 'VAR6', 'VAR7']
+    model = parse("resources/parse.py")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_r_script():
+    expected_variable_names = ['VAR1', 'VAR2', 'VAR3', 'VAR4']
+    model = parse("resources/parse.r")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_empty_python_notebook():
+    expected_variable_names = []
+    model = parse("resources/parse_python_empty.ipynb")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_empty_r_notebook():
+    expected_variable_names = []
+    model = parse("resources/parse_r_empty.ipynb")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_empty_python_script():
+    expected_variable_names = []
+    model = parse("resources/parse_empty.py")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_empty_r_script():
+    expected_variable_names = []
+    model = parse("resources/parse_empty.r")
+
+    variable_names = _get_variable_names(model)
+    assert variable_names == expected_variable_names
+
+
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError) as e:
+        parse("resources/none.py")
+    assert "not found" in str(e.value)
+
+
+def test_file_is_not_directory():
+    directory = "fakedir.py"
+    dir_path = os.path.join(os.path.dirname(__file__), directory)
+    os.mkdir(dir_path)
+
+    with pytest.raises(ValueError) as e:
+        parse(dir_path)
+    assert "is not a file" in str(e.value)
+
+    os.rmdir(dir_path)
+
+
+def test_no_kernel():
+    with pytest.raises(KeyError) as e:
+        parse("resources/parse_no_kernel.ipynb")
+    assert "No language metadata found in" in str(e.value)
+
+
+def test_parser_not_set():
+    with pytest.raises(ValueError) as e:
+        parse("resources/parse_no_language.ipynb")
+    assert "Could not find appropriate language parser" in str(e.value)
+
+
+def test_unsupported_file_type():
+    with pytest.raises(ValueError) as e:
+        parse("resources/parse.txt")
+    assert "Files with extension" and "are not supported" in str(e.value)
