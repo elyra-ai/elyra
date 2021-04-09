76aacf759f3eea855d2c1c87f0fa18bab1869453
Martha Cryan <martha.cryan@ibm.com>
2021-04-09 17:20:04 -0500

    Fix check icon

--- packages/pipeline-editor/src/theme.tsx
+++ packages/pipeline-editor/src/theme.tsx
@@ -16,7 +16,6 @@
 
 import { trashIcon } from '@elyra/ui-components';
 import {
-  checkIcon,
   closeIcon,
   editIcon,
   folderIcon,
@@ -100,7 +99,11 @@ const theme: any = {
       </SvgIcon>
     ),
     paletteIcon: LabIcon.resolveReact({ icon: paletteIcon }),
-    checkIcon: LabIcon.resolveReact({ icon: checkIcon })
+    checkIcon: (
+      <SvgIcon>
+        <path d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.051-9.506.764.646z" />
+      </SvgIcon>
+    )
   }
 };
 
