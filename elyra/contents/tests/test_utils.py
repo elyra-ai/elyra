481c396bc39a97488f8c306d35e82ccd8919cb84
Kiersten Stokes <kierstenstokes@Kierstens-MBP.attlocal.net>
2021-04-20 15:34:06 -0500

    Remove custom expected_http_error func in favor of jupyter version

--- elyra/contents/tests/test_utils.py
+++ elyra/contents/tests/test_utils.py
@@ -13,32 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import json
-import tornado
 import os
 import errno
-
-
-def expected_http_error(error, expected_code, expected_message=None):
-    """Check that the error matches the expected output error."""
-    e = error.value
-    if isinstance(e, tornado.web.HTTPError):
-        if expected_code != e.status_code:
-            return False
-        if expected_message is not None and expected_message != str(e):
-            return False
-        return True
-    elif any([
-        isinstance(e, tornado.httpclient.HTTPClientError),
-        isinstance(e, tornado.httpclient.HTTPError)
-    ]):
-        if expected_code != e.code:
-            return False
-        if expected_message:
-            message = json.loads(e.response.body.decode())['message']
-            if expected_message != message:
-                return False
-        return True
 
 
 def create_dir(location, dir_name):
