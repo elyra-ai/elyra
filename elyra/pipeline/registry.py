9cfd70997881f1ec65d9a4769e1a0c1e61d78e94
Kiersten Stokes <kiersten.stokes@ibm.com>
2021-06-17 14:21:39 -0500

    Clean up error messages

--- elyra/pipeline/registry.py
+++ elyra/pipeline/registry.py
@@ -167,11 +167,11 @@ class ComponentParser(SingletonConfigurable):
             catalog_file = os.path.join(catalog_dir, f"{self._type}_component_catalog.json")
             with open(catalog_file, 'r') as f:
                 catalog_json = json.load(f)
-        except FileNotFoundError:
-            self.log.error(f"The component catalog could not be found at path '{catalog_file}'.")
+        except FileNotFoundError as fnfe:
+            self.log.error(f"The component catalog could not be found at path '{catalog_file}': {str(fnfe)}")
             raise FileNotFoundError(f"The component catalog could not be found at path '{catalog_file}'.")
-        except Exception:
-            self.log.error(f"The component catalog at path '{catalog_file}' could not be loaded.")
+        except Exception as e:
+            self.log.error(f"The component catalog at path '{catalog_file}' could not be loaded: {str(e)}")
             raise RuntimeError(f"The component catalog at path '{catalog_file}' could not be loaded.")
 
         return catalog_json
@@ -251,6 +251,10 @@ class KfpComponentParser(ComponentParser):
 
     def __init__(self):
         super().__init__()
+
+    @property
+    def formatted_type(self):
+        return self._type.upper()
 
     def get_adjusted_parameter_fields(self,
                                       component_body,
@@ -411,6 +415,10 @@ class AirflowComponentParser(ComponentParser):
 
     def __init__(self):
         super().__init__()
+
+    @property
+    def formatted_type(self):
+        return self._type.capitalize()
 
     def parse_component_details(self, component_body, component_name=None):
         # Component_body never used, but component_name never used in KFP parser
@@ -728,8 +736,10 @@ class ComponentRegistry(SingletonConfigurable):
             # Find component with given id in component catalog
             component = parser.return_component_if_exists(component_id)
             if component is None:
-                self.log.error(f"Component with ID '{component_id}' could not be found in the component catalog.")
-                raise ValueError(f"Component with ID '{component_id}' could not be found in the component catalog.")
+                self.log.error(f"Component with ID '{component_id}' could not be found in the " +
+                               f"{parser.formatted_type} component catalog.")
+                raise ValueError(f"Component with ID '{component_id}' could not be found in the " +
+                                 f"{parser.formatted_type} component catalog.")
 
             # Get appropriate reader in order to read component definition
             reader = self._get_reader(component)
