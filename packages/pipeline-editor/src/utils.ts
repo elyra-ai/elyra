d3befd32163cc0fc972edd243e9bdaa3cef87e97
Nick Bourdakos <nicholas.bourdakos@ibm.com>
2021-06-17 14:25:24 -0400

    Remove Pipeline Editor dead/obsolete code (#1779)

--- packages/pipeline-editor/src/utils.ts
+++ packages/pipeline-editor/src/utils.ts
@@ -26,10 +26,6 @@ import pipeline_template from './pipeline-template.json';
  * A utilities class for static functions.
  */
 export default class Utils {
-  static getUUID(): any {
-    return uuid4();
-  }
-
   /**
    * Utility to create a one node pipeline to submit a single file as a pipeline
    */
@@ -45,7 +41,7 @@ export default class Utils {
     memory?: number
   ): any {
     const template = JSON.parse(JSON.stringify(pipeline_template));
-    const generated_uuid: string = Utils.getUUID();
+    const generated_uuid = uuid4();
 
     const artifactName = PathExt.basename(filename, PathExt.extname(filename));
 
@@ -73,94 +69,6 @@ export default class Utils {
     template.pipelines[0].app_data.source = PathExt.basename(filename);
 
     return template;
-  }
-
-  /**
-   * Check if the provided pipeline is empty (no nodes)
-   *
-   * @param pipelineDefinition
-   */
-  static isEmptyPipeline(pipelineDefinition: any): boolean {
-    return Object.keys(pipelineDefinition.pipelines[0].nodes).length === 0;
-  }
-
-  /**
-   * Check if the provided pipeline is clear of nodes and comments
-   *
-   * @param pipelineDefinition
-   */
-  static isEmptyCanvas(pipelineDefinition: any): boolean {
-    return (
-      this.isEmptyPipeline(pipelineDefinition) &&
-      pipelineDefinition.pipelines[0].app_data.ui_data.comments.length === 0
-    );
-  }
-
-  /**
-   * Read the version of a Pipeline. If no version is found return 0
-   *
-   * @param pipelineDefinition
-   */
-  static getPipelineVersion(pipelineDefinition: any): number {
-    let version = 0;
-
-    if (pipelineDefinition)
-      version =
-        +this.getPipelineAppdataField(
-          pipelineDefinition.pipelines[0],
-          'version'
-        ) || 0;
-
-    return version;
-  }
-
-  /**
-   * Read an application specific field from the pipeline definition
-   * (e.g. pipelines[0][app_data][fieldName])
-   */
-  static getPipelineAppdataField(node: any, fieldName: string): string {
-    if (this.hasPipelineAppdataField(node, fieldName)) {
-      return node['app_data'][fieldName] as string;
-    } else {
-      return '';
-    }
-  }
-
-  /**
-   * Check if an application specific field from the pipeline defintion exists
-   * (e.g. pipelines[0][app_data][fieldName])
-   */
-  static hasPipelineAppdataField(node: any, fieldName: string): boolean {
-    return (
-      Object.prototype.hasOwnProperty.call(node, 'app_data') &&
-      Object.prototype.hasOwnProperty.call(node['app_data'], fieldName)
-    );
-  }
-
-  /**
-   * Delete an application specific field from the pipeline definition
-   * (e.g. pipelines[0][app_data][fieldName])
-   */
-  static deletePipelineAppdataField(node: any, fieldName: string): void {
-    if (this.hasPipelineAppdataField(node, fieldName)) {
-      delete node['app_data'][fieldName];
-    }
-  }
-
-  /**
-   * Rename an application specific field from the pepileine definition if it exists by
-   * by copying the field value to the new field name and then deleting the previously
-   * existing field
-   */
-  static renamePipelineAppdataField(
-    node: any,
-    currentFieldName: string,
-    newFieldName: string
-  ): void {
-    if (this.hasPipelineAppdataField(node, currentFieldName)) {
-      node['app_data'][newFieldName] = node['app_data'][currentFieldName];
-      this.deletePipelineAppdataField(node, currentFieldName);
-    }
   }
 
   /**
