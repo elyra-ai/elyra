akchinSTC/elyra-1

Rename KFP NotebookOp to ExtendedContainerOp

elyra/kfp/operator.py
@@ -31,7 +31,7 @@ from kubernetes.client.models import V1VolumeMount
 from elyra._version import __version__
 
 """
-The NotebookOp uses a python script to bootstrap the user supplied image with the required dependencies.
+The ExtendedContainerOp uses a python script to bootstrap the user supplied image with the required dependencies.
 In order for the script run properly, the image used, must at a minimum, have the 'curl' utility available
 and have python3
 """
@@ -55,7 +55,7 @@ ELYRA_REQUIREMENTS_URL = os.getenv('ELYRA_REQUIREMENTS_URL', 'https://raw.github
                                                                     branch=ELYRA_GITHUB_BRANCH))
 
 
-class NotebookOp(ContainerOp):
+class ExtendedContainerOp(ContainerOp):
 
     def __init__(self,
                  pipeline_name: str,
@@ -265,20 +265,20 @@ class NotebookOp(ContainerOp):
         # Attach metadata to the pod
         # Node type (a static type for this op)
         self.add_pod_label('elyra/node-type',
-                           NotebookOp._normalize_label_value(
+                           ExtendedContainerOp._normalize_label_value(
                                'notebook-script'))
         # Pipeline name
         self.add_pod_label('elyra/pipeline-name',
-                           NotebookOp._normalize_label_value(self.pipeline_name))
+                           ExtendedContainerOp._normalize_label_value(self.pipeline_name))
         # Pipeline version
         self.add_pod_label('elyra/pipeline-version',
-                           NotebookOp._normalize_label_value(self.pipeline_version))
+                           ExtendedContainerOp._normalize_label_value(self.pipeline_version))
         # Experiment name
         self.add_pod_label('elyra/experiment-name',
-                           NotebookOp._normalize_label_value(self.experiment_name))
+                           ExtendedContainerOp._normalize_label_value(self.experiment_name))
         # Pipeline node name
         self.add_pod_label('elyra/node-name',
-                           NotebookOp._normalize_label_value(kwargs.get('name')))
+                           ExtendedContainerOp._normalize_label_value(kwargs.get('name')))
         # Pipeline node file
         self.add_pod_annotation('elyra/node-file-name',
                                 self.notebook)

elyra/pipeline/processor_kfp.py
@@ -39,7 +39,7 @@ from urllib3.exceptions import LocationValueError
 from urllib3.exceptions import MaxRetryError
 
 from elyra._version import __version__
-from elyra.kfp.operator import NotebookOp
+from elyra.kfp.operator import ExtendedContainerOp
 from elyra.metadata.manager import MetadataManager
 from elyra.pipeline.component_parser_kfp import KfpComponentParser
 from elyra.pipeline.pipeline import Operation
@@ -439,7 +439,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
             sanitized_operation_name = self._sanitize_operation_name(operation.name)
 
             # Create pipeline operation
-            # If operation is one of the "standard" set of NBs or scripts, construct custom NotebookOp
+            # If operation is one of the "standard" set of NBs or scripts, construct custom ExtendedContainerOp
             if operation.classifier in ["execute-notebook-node", "execute-python-node", "execute-r-node"]:
 
                 operation_artifact_archive = self._get_dependency_archive_name(operation)
@@ -447,33 +447,33 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
                 self.log.debug("Creating pipeline component:\n {op} archive : {archive}".format(
                                op=operation, archive=operation_artifact_archive))
 
-                notebook_ops[operation.id] = NotebookOp(name=sanitized_operation_name,
-                                                        pipeline_name=pipeline_name,
-                                                        experiment_name=experiment_name,
-                                                        notebook=operation.filename,
-                                                        cos_endpoint=cos_endpoint,
-                                                        cos_bucket=cos_bucket,
-                                                        cos_directory=cos_directory,
-                                                        cos_dependencies_archive=operation_artifact_archive,
-                                                        pipeline_version=pipeline_version,
-                                                        pipeline_source=pipeline.source,
-                                                        pipeline_inputs=operation.inputs,
-                                                        pipeline_outputs=operation.outputs,
-                                                        pipeline_envs=pipeline_envs,
-                                                        emptydir_volume_size=emptydir_volume_size,
-                                                        cpu_request=operation.cpu,
-                                                        mem_request=operation.memory,
-                                                        gpu_limit=operation.gpu,
-                                                        workflow_engine=engine,
-                                                        image=operation.runtime_image,
-                                                        file_outputs={
-                                                            'mlpipeline-metrics':
-                                                                '{}/mlpipeline-metrics.json'
-                                                                .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR']),
-                                                            'mlpipeline-ui-metadata':
-                                                                '{}/mlpipeline-ui-metadata.json'
-                                                                .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR'])
-                                                        })
+                notebook_ops[operation.id] = ExtendedContainerOp(name=sanitized_operation_name,
+                                                                 pipeline_name=pipeline_name,
+                                                                 experiment_name=experiment_name,
+                                                                 notebook=operation.filename,
+                                                                 cos_endpoint=cos_endpoint,
+                                                                 cos_bucket=cos_bucket,
+                                                                 cos_directory=cos_directory,
+                                                                 cos_dependencies_archive=operation_artifact_archive,
+                                                                 pipeline_version=pipeline_version,
+                                                                 pipeline_source=pipeline.source,
+                                                                 pipeline_inputs=operation.inputs,
+                                                                 pipeline_outputs=operation.outputs,
+                                                                 pipeline_envs=pipeline_envs,
+                                                                 emptydir_volume_size=emptydir_volume_size,
+                                                                 cpu_request=operation.cpu,
+                                                                 mem_request=operation.memory,
+                                                                 gpu_limit=operation.gpu,
+                                                                 workflow_engine=engine,
+                                                                 image=operation.runtime_image,
+                                                                 file_outputs={
+                                                                     'mlpipeline-metrics':
+                                                                         '{}/mlpipeline-metrics.json'
+                                                                         .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR']),  # noqa
+                                                                     'mlpipeline-ui-metadata':
+                                                                         '{}/mlpipeline-ui-metadata.json'
+                                                                         .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR'])  # noqa
+                                                                 })
 
                 # TODO Can we move all of this to apply to non-standard components as well? Test when servers are up
                 if cos_secret and not export:

elyra-ai/elyra

elyra/kfp/operator.py
@@ -31,7 +31,7 @@ from kubernetes.client.models import V1VolumeMount
 from elyra._version import __version__
 
 """
-The NotebookOp uses a python script to bootstrap the user supplied image with the required dependencies.
+The ExecuteFileOp uses a python script to bootstrap the user supplied image with the required dependencies.
 In order for the script run properly, the image used, must at a minimum, have the 'curl' utility available
 and have python3
 """
@@ -55,7 +55,7 @@ ELYRA_REQUIREMENTS_URL = os.getenv('ELYRA_REQUIREMENTS_URL', 'https://raw.github
                                                                     branch=ELYRA_GITHUB_BRANCH))
 
 
-class NotebookOp(ContainerOp):
+class ExecuteFileOp(ContainerOp):
 
     def __init__(self,
                  pipeline_name: str,
@@ -265,20 +265,20 @@ class NotebookOp(ContainerOp):
         # Attach metadata to the pod
         # Node type (a static type for this op)
         self.add_pod_label('elyra/node-type',
-                           NotebookOp._normalize_label_value(
+                           ExecuteFileOp._normalize_label_value(
                                'notebook-script'))
         # Pipeline name
         self.add_pod_label('elyra/pipeline-name',
-                           NotebookOp._normalize_label_value(self.pipeline_name))
+                           ExecuteFileOp._normalize_label_value(self.pipeline_name))
         # Pipeline version
         self.add_pod_label('elyra/pipeline-version',
-                           NotebookOp._normalize_label_value(self.pipeline_version))
+                           ExecuteFileOp._normalize_label_value(self.pipeline_version))
         # Experiment name
         self.add_pod_label('elyra/experiment-name',
-                           NotebookOp._normalize_label_value(self.experiment_name))
+                           ExecuteFileOp._normalize_label_value(self.experiment_name))
         # Pipeline node name
         self.add_pod_label('elyra/node-name',
-                           NotebookOp._normalize_label_value(kwargs.get('name')))
+                           ExecuteFileOp._normalize_label_value(kwargs.get('name')))
         # Pipeline node file
         self.add_pod_annotation('elyra/node-file-name',
                                 self.notebook)

elyra/pipeline/processor_kfp.py
@@ -39,7 +39,7 @@ from urllib3.exceptions import LocationValueError
 from urllib3.exceptions import MaxRetryError
 
 from elyra._version import __version__
-from elyra.kfp.operator import NotebookOp
+from elyra.kfp.operator import ExecuteFileOp
 from elyra.metadata.manager import MetadataManager
 from elyra.pipeline.component_parser_kfp import KfpComponentParser
 from elyra.pipeline.pipeline import Operation
@@ -439,7 +439,7 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
             sanitized_operation_name = self._sanitize_operation_name(operation.name)
 
             # Create pipeline operation
-            # If operation is one of the "standard" set of NBs or scripts, construct custom NotebookOp
+            # If operation is one of the "standard" set of NBs or scripts, construct custom ExecuteFileOp
             if operation.classifier in ["execute-notebook-node", "execute-python-node", "execute-r-node"]:
 
                 operation_artifact_archive = self._get_dependency_archive_name(operation)
@@ -447,33 +447,33 @@ class KfpPipelineProcessor(RuntimePipelineProcessor):
                 self.log.debug("Creating pipeline component:\n {op} archive : {archive}".format(
                                op=operation, archive=operation_artifact_archive))
 
-                notebook_ops[operation.id] = NotebookOp(name=sanitized_operation_name,
-                                                        pipeline_name=pipeline_name,
-                                                        experiment_name=experiment_name,
-                                                        notebook=operation.filename,
-                                                        cos_endpoint=cos_endpoint,
-                                                        cos_bucket=cos_bucket,
-                                                        cos_directory=cos_directory,
-                                                        cos_dependencies_archive=operation_artifact_archive,
-                                                        pipeline_version=pipeline_version,
-                                                        pipeline_source=pipeline.source,
-                                                        pipeline_inputs=operation.inputs,
-                                                        pipeline_outputs=operation.outputs,
-                                                        pipeline_envs=pipeline_envs,
-                                                        emptydir_volume_size=emptydir_volume_size,
-                                                        cpu_request=operation.cpu,
-                                                        mem_request=operation.memory,
-                                                        gpu_limit=operation.gpu,
-                                                        workflow_engine=engine,
-                                                        image=operation.runtime_image,
-                                                        file_outputs={
-                                                            'mlpipeline-metrics':
-                                                                '{}/mlpipeline-metrics.json'
-                                                                .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR']),
-                                                            'mlpipeline-ui-metadata':
-                                                                '{}/mlpipeline-ui-metadata.json'
-                                                                .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR'])
-                                                        })
+                notebook_ops[operation.id] = ExecuteFileOp(name=sanitized_operation_name,
+                                                           pipeline_name=pipeline_name,
+                                                           experiment_name=experiment_name,
+                                                           notebook=operation.filename,
+                                                           cos_endpoint=cos_endpoint,
+                                                           cos_bucket=cos_bucket,
+                                                           cos_directory=cos_directory,
+                                                           cos_dependencies_archive=operation_artifact_archive,
+                                                           pipeline_version=pipeline_version,
+                                                           pipeline_source=pipeline.source,
+                                                           pipeline_inputs=operation.inputs,
+                                                           pipeline_outputs=operation.outputs,
+                                                           pipeline_envs=pipeline_envs,
+                                                           emptydir_volume_size=emptydir_volume_size,
+                                                           cpu_request=operation.cpu,
+                                                           mem_request=operation.memory,
+                                                           gpu_limit=operation.gpu,
+                                                           workflow_engine=engine,
+                                                           image=operation.runtime_image,
+                                                           file_outputs={
+                                                               'mlpipeline-metrics':
+                                                                   '{}/mlpipeline-metrics.json'
+                                                                   .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR']),  # noqa
+                                                               'mlpipeline-ui-metadata':
+                                                                   '{}/mlpipeline-ui-metadata.json'
+                                                                   .format(pipeline_envs['ELYRA_WRITABLE_CONTAINER_DIR'])  # noqa
+                                                           })
 
                 # TODO Can we move all of this to apply to non-standard components as well? Test when servers are up
                 if cos_secret and not export: