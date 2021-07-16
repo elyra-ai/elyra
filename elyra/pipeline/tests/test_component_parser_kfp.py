b9527e34c1cf15a44b5b3e44268d47ba232d4c1c
Kiersten Stokes <kiersten.stokes@ibm.com>
2021-07-16 12:44:24 -0500

    Add elyra_ prefix to newly merged test case

--- elyra/pipeline/tests/test_component_parser_kfp.py
+++ elyra/pipeline/tests/test_component_parser_kfp.py
@@ -77,15 +77,15 @@ def test_parse_kfp_component_file():
     # Ensure that the 'required' attribute was set correctly. KFP components default to required
     # unless explicitly marked otherwise in component YAML.
     required_property = next(prop for prop in properties_json['uihints']['parameter_info']
-                             if prop.get('parameter_ref') == 'test_required_property')
+                         if prop.get('parameter_ref') == 'elyra_test_required_property')
     assert required_property['data']['required'] is True
 
     optional_property = next(prop for prop in properties_json['uihints']['parameter_info']
-                             if prop.get('parameter_ref') == 'test_optional_property')
+                             if prop.get('parameter_ref') == 'elyra_test_optional_property')
     assert optional_property['data']['required'] is False
 
     default_required_property = next(prop for prop in properties_json['uihints']['parameter_info']
-                                     if prop.get('parameter_ref') == 'test_required_property_default')
+                                     if prop.get('parameter_ref') == 'elyra_test_required_property_default')
     assert default_required_property['data']['required'] is True
 
 
