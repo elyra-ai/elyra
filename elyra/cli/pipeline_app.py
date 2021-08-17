500a8c11a89ea264ce99c15f62614160c83e2d42
Luciano Resende <lresende@us.ibm.com>
2021-08-17 08:26:13 -0700

    Simplify as suggested on review

--- elyra/cli/pipeline_app.py
+++ elyra/cli/pipeline_app.py
@@ -50,19 +50,6 @@ def _get_runtime_type(runtime_config: Optional[str]) -> Optional[str]:
         raise click.ClickException(f'Invalid runtime configuration: {runtime_config}\n {e}')
 
 
-def _get_runtime_display_name_from_config(runtime_config: Optional[str]) -> Optional[str]:
-    if not runtime_config:
-        return None
-
-    try:
-        schema_manager = SchemaManager.instance()
-        schema_name = _get_runtime_type(runtime_config)
-        schema = schema_manager.get_schema('runtimes', schema_name)
-        return schema['display_name']
-    except Exception as e:
-        raise click.ClickException(f'Invalid runtime configuration: {runtime_config}\n {e}')
-
-
 def _get_runtime_display_name(schema_name: Optional[str]) -> Optional[str]:
     if not schema_name:
         return None
@@ -151,7 +138,7 @@ def _preprocess_pipeline(pipeline_path: str,
 
     if not _validate_pipeline_runtime(primary_pipeline, runtime):
         runtime_description = _get_runtime_display_name(primary_pipeline['app_data']['runtime'])
-        runtime_config_display_name = _get_runtime_display_name_from_config(runtime_config)
+        runtime_config_display_name = _get_runtime_display_name(_get_runtime_type(runtime_config))
         raise click.ClickException(
             f"This pipeline requires an instance of {runtime_description} runtime configuration.\n"
             f"The specified configuration '{runtime_config}' is for {runtime_config_display_name} runtime.")
