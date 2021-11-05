b968aa6d1082a04d939d29a8a580759a1badc4a0
Nick Bourdakos <bourdakos1@gmail.com>
2021-11-05 10:53:37 -0400

    use top level label to sort

--- packages/pipeline-editor/src/pipeline-hooks.ts
+++ packages/pipeline-editor/src/pipeline-hooks.ts
@@ -85,6 +85,7 @@ export interface IRuntimeComponent {
   node_types: {
     op: string;
     id: string;
+    label: string;
     type: 'execution_node';
     inputs: { app_data: any }[];
     outputs: { app_data: any }[];
@@ -138,13 +139,9 @@ export const sortPalette = (palette: {
 
   for (const components of palette.categories) {
     components.node_types.sort((a, b) =>
-      a.app_data.ui_data.label.localeCompare(
-        b.app_data.ui_data.label,
-        undefined,
-        {
-          numeric: true
-        }
-      )
+      a.label.localeCompare(b.label, undefined, {
+        numeric: true
+      })
     );
   }
 };
