c6e5c6687112870932f8c3d7d4f80eea08b2e181
Kevin Bates <kevin.bates@us.ibm.com>
2021-11-17 10:05:44 -0800

    Fix test

--- elyra/tests/cli/test_pipeline_app.py
+++ elyra/tests/cli/test_pipeline_app.py
@@ -322,7 +322,7 @@ def test_describe_with_kfp_components():
 
 
 @pytest.mark.parametrize('component_cache_instance', [KFP_COMPONENT_CACHE_INSTANCE], indirect=True)
-def test_validate_with_kfp_components(monkeypatch, component_cache_instance):
+def test_validate_with_kfp_components(kfp_runtime_instance, component_cache_instance):
     runner = CliRunner()
     pipeline_file_path = os.path.join(os.path.dirname(__file__), 'resources', 'kfp_3_node_custom.pipeline')
 
