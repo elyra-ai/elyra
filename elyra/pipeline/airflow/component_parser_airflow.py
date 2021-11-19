34d7adf747665989b8bd2ab70b6cfc26c2973696
Alan Chin <akchin@us.ibm.com>
2021-11-19 11:34:02 -0800

    Set the default xcom_push for Airflow operators

--- elyra/pipeline/airflow/component_parser_airflow.py
+++ elyra/pipeline/airflow/component_parser_airflow.py
@@ -127,6 +127,10 @@ class AirflowComponentParser(ComponentParser):
             description = self._format_description(description=description, data_type=data_type)
 
             data_type_info = self.determine_type_information(data_type)
+
+            if 'xcom' in arg.lower() and data_type_info.data_type == 'boolean':
+                data_type_info.default_value = True
+
             if data_type_info.undetermined:
                 self.log.debug(f"Data type from parsed data ('{data_type}') could not be determined. "
                                f"Proceeding as if 'string' was detected.")
