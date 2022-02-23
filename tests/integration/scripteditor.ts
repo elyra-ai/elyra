ad872eab8e1888f0fac75000febc534e8f39fcbb
Hema-Mathiyazhagan <99223064+Hema-Mathiyazhagan@users.noreply.github.com>
2022-02-23 18:20:34 -0500

    Adding UI prettier suggestions

--- tests/integration/scripteditor.ts
+++ tests/integration/scripteditor.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 describe('Script Editor tests', () => {
   before(() => {
     cy.resetJupyterLab();
