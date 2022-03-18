0cf3c1775bc3690272521ac3903ed7a6a4a48d36
Martha Cryan <Martha.Cryan@ibm.com>
2022-03-18 15:00:11 -0500

    Style password button

--- packages/metadata-common/src/PasswordField.tsx
+++ packages/metadata-common/src/PasswordField.tsx
@@ -5,22 +5,20 @@ import { viewIcon, viewOffIcon } from '@elyra/ui-components';
 export const PasswordField: Field = props => {
   const { StringField } = props.registry.fields;
   const [showPassword, setShowPassword] = React.useState(false);
-  const onChange = (e: any, es: any) => {
-    console.log(e);
-    props.onChange(e, es);
-  };
 
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
       <StringField
         {...props}
         uiSchema={{
           ...props.uiSchema,
           'ui:widget': showPassword ? undefined : 'password'
         }}
-        onChange={onChange}
       />
-      <button onClick={() => setShowPassword(!showPassword)}>
+      <button
+        className="elyra-passwordFieldButton"
+        onClick={() => setShowPassword(!showPassword)}
+      >
         {showPassword ? <viewOffIcon.react /> : <viewIcon.react />}
       </button>
     </div>
