77a43b2ce26d487155beb033a2fee88b8457aea5
Kiersten Stokes <kiersten.stokes@ibm.com>
2022-03-25 14:55:51 -0500

    Add comment in metadata post_hook exceptions

--- elyra/pipeline/component_metadata.py
+++ elyra/pipeline/component_metadata.py
@@ -45,12 +45,18 @@ class ComponentCatalogMetadata(Metadata):
         try:  # Modify components associated with this catalog on creates and updates.
             component_catalog.ComponentCache.instance().update(catalog=self, action='modify')
         except Exception:
+            # An attempted component cache update will fail silently with most errors
+            # logged from the ComponentCache class methods. However, a log message
+            # should eventually be added here as well
             pass
 
     def post_delete(self, **kwargs: Any) -> None:
         try:  # Remove components associated with this catalog on deletes.
             component_catalog.ComponentCache.instance().update(catalog=self, action='delete')
         except Exception:
+            # An attempted component cache update will fail silently with most errors
+            # logged from the ComponentCache class methods. However, a log message
+            # should eventually be added here as well
             pass
 
 
