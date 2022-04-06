61eb47d1dfb274bf1e3d9f589616dc9de07c8f22
Alex Bozarth <ajbozart@us.ibm.com>
2022-04-06 16:11:13 -0500

    add tent. support for jupyterlab#12195
    https://github.com/jupyterlab/jupyterlab/pull/12195

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -27,7 +27,7 @@ import { ITranslator } from '@jupyterlab/translation';
 import { launcherIcon } from '@jupyterlab/ui-components';
 
 import { toArray } from '@lumino/algorithm';
-import { Widget } from '@lumino/widgets';
+import { DockPanel, TabBar, Widget } from '@lumino/widgets';
 
 import { Launcher, LauncherModel } from './launcher';
 import '../style/index.css';
@@ -80,7 +80,7 @@ const extension: JupyterFrontEndPlugin<ILauncher> = {
     }
 
     // Use custom Elyra launcher
-    const { commands } = app;
+    const { commands, shell } = app;
     const trans = translator.load('jupyterlab');
     const model = new LauncherModel();
 
@@ -111,7 +111,10 @@ const extension: JupyterFrontEndPlugin<ILauncher> = {
         main.title.closable = !!toArray(labShell.widgets('main')).length;
         main.id = id;
 
-        labShell.add(main, 'main', { activate: args['activate'] as boolean });
+        shell.add(main, 'main', {
+          activate: args['activate'] as boolean,
+          ref: args['ref'] as string
+        });
 
         labShell.layoutModified.connect(() => {
           // If there is only a launcher open, remove the close icon.
@@ -127,6 +130,25 @@ const extension: JupyterFrontEndPlugin<ILauncher> = {
         command: CommandIDs.create,
         category: trans.__('Launcher')
       });
+    }
+
+    if (labShell) {
+      labShell.addButtonEnabled = true;
+      labShell.addRequested.connect(
+        (sender: DockPanel, arg: TabBar<Widget>) => {
+          // Get the ref for the current tab of the tabbar which the add button was clicked
+          const ref =
+            arg.currentTitle?.owner.id ||
+            arg.titles[arg.titles.length - 1].owner.id;
+          if (commands.hasCommand('filebrowser:create-main-launcher')) {
+            // If a file browser is defined connect the launcher to it
+            return commands.execute('filebrowser:create-main-launcher', {
+              ref
+            });
+          }
+          return commands.execute(CommandIDs.create, { ref });
+        }
+      );
     }
 
     commands.addCommand(CommandIDs.openHelp, {
