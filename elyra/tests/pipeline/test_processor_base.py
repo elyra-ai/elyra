3f0a66ba3fa86b80aebc691664a40f6f4e67eb10
Alan Chin <akchin@us.ibm.com>
2022-04-28 13:51:57 -0700

    Fix volume mount unit test

--- elyra/tests/pipeline/test_processor_base.py
+++ elyra/tests/pipeline/test_processor_base.py
@@ -19,6 +19,7 @@ import pytest
 
 from elyra.pipeline.parser import PipelineParser
 from elyra.pipeline.pipeline import GenericOperation
+from elyra.pipeline.pipeline import KeyValueList
 from elyra.pipeline.processor import RuntimePipelineProcessor
 from elyra.tests.pipeline.test_pipeline_parser import _read_pipeline_resource
 
@@ -49,12 +50,11 @@ def sample_metadata():
 
 
 def test_get_volume_mounts(runtime_processor):
-    component_parameters = {
-        "filename": "pipelines_test_file",
-        "env_vars": [],
-        "runtime_image": "tensorflow/tensorflow:latest",
-        "mounted_volumes": ["/mount/test=rwx-test-claim", "/mount/test_two=second-claim"],
-    }
+    component_parameters = {"filename": "pipelines_test_file", "env_vars": [],
+                            "runtime_image": "tensorflow/tensorflow:latest",
+                            "mounted_volumes": KeyValueList(["/mount/test=rwx-test-claim",
+                                                             "/mount/test_two=second-claim"])}
+
     test_operation = GenericOperation(
         id="this-is-a-test-id",
         type="execution-node",
@@ -62,6 +62,7 @@ def test_get_volume_mounts(runtime_processor):
         name="test",
         component_params=component_parameters,
     )
+
     parsed_volumes_dict = runtime_processor._get_volume_mounts(operation=test_operation)
     assert parsed_volumes_dict == {
         "/mount/test": "rwx-test-claim",
