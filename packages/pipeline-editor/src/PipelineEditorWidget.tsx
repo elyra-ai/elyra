9ecbf5fad60db6e9c35414795b412d6bb51b2f62
Alex Bozarth <ajbozart@us.ibm.com>
2022-05-03 17:48:22 -0500

    Update to JupyterLab 3.4 (#2632)
    Co-authored-by: Martha Cryan <Martha.Cryan@ibm.com>

--- packages/pipeline-editor/src/PipelineEditorWidget.tsx
+++ packages/pipeline-editor/src/PipelineEditorWidget.tsx
@@ -1036,7 +1036,7 @@ const PipelineWrapper: React.FC<IProps> = ({
   };
 
   const handleOpenSettings = (): void => {
-    commands.execute('settingeditor:open');
+    commands.execute('settingeditor:open', { query: 'Pipeline Editor' });
   };
 
   return (
