138c80123515a1f638b9eaf68f667f68857d02e8
Stefan Zecevic <szecevic@ibm.com>
2022-05-24 15:03:09 -0700

    Revert "Add elyra documentation link in elyra-metadata help (#2731)"
    This reverts commit 8f3686399e1408e5f09c74a1b6a1e63ce4a71eff.

--- elyra/metadata/metadata_app_utils.py
+++ elyra/metadata/metadata_app_utils.py
@@ -640,10 +640,10 @@ class AppBase(object):
         print("-----------")
         print("Subcommands are launched as `elyra-metadata cmd [args]`. For information on")
         print("using subcommand 'cmd', run: `elyra-metadata cmd -h` or `elyra-metadata cmd --help`.")
-        print("\nFind more information at https://elyra.readthedocs.io/en/latest/")
         print()
         for subcommand, desc in self.subcommands.items():
-            print(f"{subcommand:<10}{desc[1]:>10}")
+            print(subcommand)
+            print(f"    {desc[1]}")
 
     @staticmethod
     def exit(status: int):
