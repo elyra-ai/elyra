d52642f23f70cd41b346fed2216ea47dfdb34e66
Karla Spuldaro <karla.spuldaro@ibm.com>
2022-06-10 15:51:22 -0400

    Track active session connections from plugin

--- packages/script-debugger/src/index.ts
+++ packages/script-debugger/src/index.ts
@@ -20,10 +20,15 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+// import { Dialog, showDialog } from '@jupyterlab/apputils';
 import { Debugger, IDebugger } from '@jupyterlab/debugger';
 import { DocumentWidget } from '@jupyterlab/docregistry';
 import { IEditorTracker } from '@jupyterlab/fileeditor';
-// import { Session } from '@jupyterlab/services';
+import {
+  // KernelManager,
+  Session
+  // SessionManager
+} from '@jupyterlab/services';
 
 /**
  * Debugger plugin.
@@ -48,61 +53,91 @@ const scriptEditorDebuggerExtension: JupyterFrontEndPlugin<void> = {
       service: debug
     });
 
-    //   const activeSessions: {
-    //     [id: string]: Session.ISessionConnection;
-    //   } = {};
+    const activeSessions: { [id: string]: Session.ISessionConnection } = {};
+    // const kernelManager = new KernelManager();
+    // const sessionManager = new SessionManager({
+    //   kernelManager: kernelManager
+    // });
+    const sessionConnection: Session.ISessionConnection | null = null;
 
     const updateHandlerAndCommands = async (
-      widget: DocumentWidget
+      widget: ScriptEditor
     ): Promise<void> => {
-      // TODO: get kernel selection from widget
-      const scriptEditorWidget = widget as ScriptEditor;
-      const kernelSelection = scriptEditorWidget.getKernelSelection();
-      console.log('kernelSelection: ' + kernelSelection);
+      const kernelSelection = widget.getKernelSelection();
+      console.log(
+        'from updateHandlerAndCommands:\nkernelSelection: ' + kernelSelection
+      );
 
-      // TODO: start a kernel session
-      const session = null;
-      await handler.update(widget, session);
-      app.commands.notifyCommandChanged();
+      const sessions = app.serviceManager.sessions;
+      try {
+        const model = await sessions.findByPath(widget.context.path);
+        if (!model) {
+          return;
+        }
+        let session = activeSessions[model.id];
+        if (!session) {
+          // Use `connectTo` only if the session does not exist.
+          // `connectTo` sends a kernel_info_request on the shell
+          // channel, which blocks the debug session restore when waiting
+          // for the kernel to be ready
+          session = sessions.connectTo({ model });
+          activeSessions[model.id] = session;
+        }
+        await handler.update(widget, session);
+        app.commands.notifyCommandChanged();
+      } catch {
+        return;
+      }
 
-      // const sessions = app.serviceManager.sessions;
-      // try {
-      //   const model = await sessions.findByPath(widget.context.path);
-      //   if (!model) {
-      //     return;
+      // Start a kernel session
+      // if (!sessionConnection) {
+      //   // this.disableButton(true, 'debug');
+      //   try {
+      //     await startSession(kernelSelection, widget.context.path);
+      //     console.log('Kernel session started for ' + kernelSelection);
+      //   } catch (e) {
+      //     showDialog({
+      //       title: 'Error',
+      //       body: 'Could not start session to debug script',
+      //       buttons: [Dialog.okButton()]
+      //     });
       //   }
-      //   let session = activeSessions[model.id];
-      //   if (!session) {
-      //     // Use `connectTo` only if the session does not exist.
-      //     // `connectTo` sends a kernel_info_request on the shell
-      //     // channel, which blocks the debug session restore when waiting
-      //     // for the kernel to be ready
-      //     session = sessions.connectTo({ model });
-      //     activeSessions[model.id] = session;
-      //   }
-      //   await handler.update(widget, session);
-      //   app.commands.notifyCommandChanged();
-      // } catch {
-      //   return;
       // }
+      await handler.update(widget, sessionConnection);
+      app.commands.notifyCommandChanged();
     };
 
     if (labShell) {
       labShell.currentChanged.connect((_, update) => {
         const widget = update.newValue;
-        if (widget instanceof ScriptEditor) {
+        if (widget instanceof ScriptEditor && widget instanceof ScriptEditor) {
           void updateHandlerAndCommands(widget);
         }
       });
     } else {
-      editorTracker.currentChanged.connect((_, documentWidget) => {
-        if (documentWidget) {
-          void updateHandlerAndCommands(
-            (documentWidget as unknown) as DocumentWidget
-          );
+      editorTracker.currentChanged.connect((_, widget) => {
+        if (widget) {
+          (widget as unknown) as DocumentWidget;
         }
       });
     }
+
+    // const startSession = async (
+    //   kernelName: string,
+    //   contextPath: string
+    // ): Promise<Session.ISessionConnection> => {
+    //   const options: Session.ISessionOptions = {
+    //     kernel: {
+    //       name: kernelName
+    //     },
+    //     path: contextPath,
+    //     type: 'file',
+    //     name: contextPath
+    //   };
+    //   sessionConnection = await sessionManager.startNew(options);
+    //   sessionConnection.setPath(contextPath);
+    //   return sessionConnection;
+    // };
   }
 };
 
