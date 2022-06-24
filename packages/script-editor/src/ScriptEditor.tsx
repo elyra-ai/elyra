120ad22720b8a59b8c49d1ce4842bc3e6d3811f9
VNA818-RPI <Arcerv@rpi.edu>
2022-06-23 23:18:11 -0400

    Update ScriptEditor.tsx

--- packages/script-editor/src/ScriptEditor.tsx
+++ packages/script-editor/src/ScriptEditor.tsx
@@ -227,11 +227,12 @@ export abstract class ScriptEditor extends DocumentWidget<
       return;
     } else if (msg.error) {
       output = 'Error : ' + msg.error.type + ' - ' + msg.error.output;
+      this.displayOutput(output);
       this.getOutputAreaChildWidget().addClass(OUTPUT_AREA_ERROR_CLASS);
     } else if (msg.output) {
       output = msg.output;
+      this.displayOutput(output);
     }
-    this.displayOutput(output);
   };
 
   private createScrollButtons = (
