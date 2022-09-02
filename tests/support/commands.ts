1bf9b071df6da6ba01800710536bbb2c1cb0641b
Karla Spuldaro <karla.spuldaro@ibm.com>
2022-09-02 16:27:34 -0400

    Fix typo

--- tests/support/commands.ts
+++ tests/support/commands.ts
@@ -82,7 +82,7 @@ Cypress.Commands.add('createRuntimeConfig', ({ type } = {}): void => {
 
   cy.findByLabelText(/object storage endpoint/i).type('http://0.0.0.0:9000');
 
-  if (type !== 'invald') {
+  if (type !== 'invalid') {
     cy.findByLabelText(/object storage username/i).type('minioadmin');
     cy.findByLabelText(/object storage password/i).type('minioadmin');
   }
