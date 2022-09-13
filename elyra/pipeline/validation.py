2934536f81f87eb027a467bd029980c92cd34c4a
Kiersten Stokes <kierstenstokes@gmail.com>
2022-09-13 10:28:45 -0500

    Validate required file types do not have empty string as value

--- elyra/pipeline/validation.py
+++ elyra/pipeline/validation.py
@@ -581,6 +581,13 @@ class PipelineValidationManager(SingletonConfigurable):
                             filename=filename,
                             response=response,
                         )
+                    elif self._is_required_property(component_property_dict, default_parameter):
+                        response.add_message(
+                            severity=ValidationSeverity.Error,
+                            message_type="invalidNodeProperty",
+                            message="Node is missing a value for a required property.",
+                            data={"nodeID": node.id, "nodeName": node.label, "propertyName": default_parameter},
+                        )
 
     def _validate_container_image_name(
         self, node_id: str, node_label: str, image_name: str, response: ValidationResponse
