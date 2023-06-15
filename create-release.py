9ebf8d6c95eb821e2f7f4d4225c5c5763241eede
Fellipe Resende <fellipe.barros.resende@gmail.com>
2023-06-14 21:18:42 -0700

    Update Copyright year (#3162)
    Signed-off-by: Fellipe Resende <fellipe.barros.resende@gmail.com>

--- create-release.py
+++ create-release.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2018-2022 Elyra Authors
+# Copyright 2018-2023 Elyra Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
