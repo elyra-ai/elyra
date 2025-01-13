166c7bcc1afa03d071cf3d7b4b7a32cfbd379787
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2025-01-13 16:31:58 +0000

    Remove Python 3.8 support as it reached EOL
    Signed-off-by: Guilherme Caponetto <638737+caponetto@users.noreply.github.com>

--- elyra/kfp/bootstrapper.py
+++ elyra/kfp/bootstrapper.py
@@ -635,7 +635,7 @@ class OpUtil(object):
     @classmethod
     def determine_elyra_requirements(cls) -> Any:
         if sys.version_info.major == 3:
-            if sys.version_info.minor in [8, 9, 10, 11]:
+            if sys.version_info.minor in [9, 10, 11]:
                 return "requirements-elyra.txt"
         logger.error(
             f"This version of Python '{sys.version_info.major}.{sys.version_info.minor}' "
