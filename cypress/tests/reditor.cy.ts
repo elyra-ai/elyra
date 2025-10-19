c17edc45ecf549526c47cc4d09398cc5fb4ddfcc
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2025-10-19 22:16:14 +0100

    fix: tests needed to be updated
    Signed-off-by: Jordan-Williams2 <Jordan.Williams2@ibm.com>

--- cypress/tests/reditor.cy.ts
+++ cypress/tests/reditor.cy.ts
@@ -54,8 +54,8 @@ describe('R Editor tests', () => {
 
   it('check icons', () => {
     // Check file menu editor contents
-    cy.get('.jp-MenuBar-item[data-command="filemenu:open"]').click();
-    cy.get('[data-command="filemenu:new"] .lm-Menu-itemLabel').click();
+    cy.get('.lm-MenuBar-itemLabel:contains("File")').click();
+    cy.get('.lm-Menu-itemLabel:contains("New")').click();
     cy.get(
       '[data-command="script-editor:create-new-r-editor"] svg[data-icon="elyra:rIcon"]'
     );
@@ -71,8 +71,8 @@ describe('R Editor tests', () => {
   });
 
   it('opens blank R file from menu', () => {
-    cy.get('.jp-MenuBar-item[data-command="filemenu:open"]').click();
-    cy.get('[data-command="filemenu:new"] .lm-Menu-itemLabel').click();
+    cy.get('.lm-MenuBar-itemLabel:contains("File")').click();
+    cy.get('.lm-Menu-itemLabel:contains("New")').click();
 
     cy.get(
       '[data-command="script-editor:create-new-r-editor"] > .lm-Menu-itemLabel'
